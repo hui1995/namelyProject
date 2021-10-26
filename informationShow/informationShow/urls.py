52c73d0e449f2cbfab5ded700b0beee32bcfb0cf
breathcoder <breathcoder@gmail.com>
2021-10-26 23:24:03 +0800

    代码提交

--- informationShow/informationShow/urls.py
+++ informationShow/informationShow/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myApp.views import HomeView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('admin/', admin.site.urls),
+  path('',HomeView.as_view()),
 ]
--- informationShow/myApp/views.py
+++ informationShow/myApp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views import View
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+
 
 # Create your views here.
+
+class HomeView(View):
+    def get(self,request):
+        return render(request,'home.html')
+
+    def post(self,request):
+        pass
+        return redirect("")
