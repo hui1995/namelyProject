b80013a4469532d9e989f32599db813f83335abc
zhaohui <zhaohui@cc316.com>
2021-11-07 15:26:48 +0800

    登陆相关功能开发

--- informationShow/myApp/models.py
+++ informationShow/myApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import TextField
+from django.db.models.fields.related import ForeignKey
+
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CommentsModel(models.Model):
+    content=TextField()
+    user=ForeignKey(AbstractUser,on_delete=models.CASCADE)
--- informationShow/myApp/views.py
+++ informationShow/myApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.views import View
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+from django.contrib.auth import authenticate, login
 
 
 
@@ -26,6 +27,23 @@ class MemebersView(View):
 class LoginView(View):
     def get(self,request):
         return render(request,'login.html')
+
+    def post(self,request):
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        if username is None or password is None:
+            return render(request,"login.html",message="用户吗和密码不能为空")
+        user = authenticate(request, username=username, password=password)      
+        # 验证如果用户不为空
+        if user is not None:
+            # login方法登录
+            login(request, user)
+            # 返回登录成功信息
+            return redirect("/")
+        else:
+            # 返回登录失败信息
+            return render(request,"login.html",message="用户名或者密码错误")
+
 
 class MessageView(View):
     def get(self,request):
